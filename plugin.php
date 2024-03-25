931c2009cc98f3e0deb8124ec1e26797fed91560
Caleb Zhang <132726706+calzh@users.noreply.github.com>
2024-03-25 16:30:14 +0100

    feat: wechat tip

--- plugin.php
+++ plugin.php
@@ -1,149 +1,92 @@
 <?php
 /*
-Plugin Name: typer
-Plugin URI: http://5th.li/typer_
-Description: this plugin will ask the visitor to type in the URL by himself, if the requested URL is trailed by the character _
-Version: 0.1
-Author: @koma5
-Author URI: http://5th.ch/
+Plugin Name: typer_wechat
+Description: This plugin will detect if the request is from WeChat's built-in browser and ask the visitor to open in a regular browser. If not in WeChat, it shows a dynamic countdown before redirecting.
+Version: 0.4
+Author: @calzh
 */
 
-define( 'TYPER_PAGE_CHAR', '_' );
+define('TYPER_PAGE_CHAR', '_');
+define('WECHAT_UA_STRING', 'MicroMessenger');
 
-// do something, if yourls doesn't recognise a valid short link
-yourls_add_action( 'loader_failed', 'typer_detect_request' );
+// Hook into YOURLS's loader_failed action
+yourls_add_action('loader_failed', 'typer_detect_request');
 
-function typer_detect_request($args) 
-{
-        $request = $args[0];
-        $pattern = yourls_make_regexp_pattern( yourls_get_shorturl_charset() );
-        if( preg_match( "@^([$pattern]+)".TYPER_PAGE_CHAR."$@", $request, $matches ) ) {
-                $keyword = isset( $matches[1] ) ? $matches[1] : '';
-                $keyword = yourls_sanitize_keyword( $keyword );
-                if( yourls_is_shorturl( $keyword ) ) {
-		        typer_show_page( $keyword );
-		        die();
-		}
+function typer_detect_request($args) {
+    $request = $args[0];
+    $pattern = yourls_make_regexp_pattern(yourls_get_shorturl_charset());
+    if (preg_match("@^([$pattern]+)" . TYPER_PAGE_CHAR . "$@", $request, $matches)) {
+        $keyword = isset($matches[1]) ? $matches[1] : '';
+        $keyword = yourls_sanitize_keyword($keyword);
+        if (yourls_is_shorturl($keyword)) {
+            // Detect if the user is using WeChat's built-in browser
+            if (strpos($_SERVER['HTTP_USER_AGENT'], WECHAT_UA_STRING) !== false) {
+                typer_show_wechat_page($keyword);
+            } else {
+                typer_show_countdown_page($keyword);
+            }
+            die();
         }
+    }
 }
 
-function typer_show_page($keyword)
-{
-       $urlBase  = YOURLS_SITE;
-        
-echo <<<HTML
+function typer_show_wechat_page($keyword) {
+    // CSS and HTML for WeChat warning, fully based on your provided document
+    echo <<<HTML
 <!DOCTYPE html>
 <html>
-  <head>
-  
-  <meta charset="utf-8">
-  <title>type it!</title>
-  
-  <style type="text/css">
-//global reset
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure, 
-footer, header, hgroup, menu, nav, section {
-	display: block;
-}
-body {
-	line-height: 1;
-}
-ol, ul {
-	list-style: none;
-}
-blockquote, q {
-	quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-	content: '';
-	content: none;
-}
-table {
-	border-collapse: collapse;
-	border-spacing: 0;
-}
-
-body
-{
-	background-color: #BABABA;
-	color: #4D4D4D;
-	font-family: Verdana;
-
-}
-
-div#blubb
-{
-	position: fixed;
-	top: 120px;
-	left: 45px;
-	width:500px;
-	text-align: left;
-}
-  
-  </style>
-  
-  <script type="text/javascript">
-
-	//courtesy of BoogieJack.com
-	function killCopy(e){
-	return false
-	}
-	function reEnable(){
-	return true
-	}
-	document.onselectstart=new Function ("return false")
-	if (window.sidebar){
-	document.onmousedown=killCopy
-	document.onclick=reEnable
-	}
-
-	window.onload = function ()
-	{
-	window.history.replaceState(null, '.', '.');
-	}
-
-  
-  </script>
-  </head>
-  
-  <body>
-  
-    <div id="blubb">
-	<h1>$urlBase/$keyword</h1>
-    
-<p>dear lazy visitor you landed on a page where you won't be redirected automatically. The person who sent you this link may think you could yet again do something for your links. For this purpos you have to type in the short URL.
-</p>
-<p>PS: Bots won't be able to figure out... at least till now ;)</p>
-    
-    </div>
-    
-  </body>
+<head>
+    <meta charset="utf-8">
+    <title>请在浏览器中打开</title>
+    <style type="text/css">
+        body { font:normal 14px/1.5 Arial, Microsoft Yahei; color:#333; background-color: #f4f4f4; }
+        .wxtip { background: rgba(0,0,0,0.8); text-align: center; position: fixed; left:0; top: 0; width: 100%; height: 100%; z-index: 998; display: block; }
+        .wxtip-icon { width: 104px; height: 134px; background: url('weixin-tip.png') no-repeat; display: block; position: fixed; right: 60px; top: 20px;background-size: contain; }
+        .wxtip-txt { padding-top: 107px; color: #fff; font-size: 60px; line-height: 1.5; }
+    </style>
+</head>
+<body>
+<div class="wxtip" id="JweixinTip">
+    <span class="wxtip-icon"></span>
+    <p class="wxtip-txt">点击右上角<br/>选择在默认浏览器中打开</p>
+</div>
+</body>
 </html>
 HTML;
-
-
 }
 
+function typer_show_countdown_page($keyword) {
+    $redirectUrl = YOURLS_SITE . '/' . $keyword;
+    // HTML and JavaScript for dynamic countdown
+    echo <<<HTML
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>跳转中……</title>
+    <style type="text/css">
+        body { font:normal 14px/1.5 Arial, Microsoft Yahei; color:#333; text-align: center; padding-top: 20px; background-color: #f4f4f4; }
+        .countdown-text { font-size: 48px; }
+    </style>
+</head>
+<body>
+<div class="countdown">
+    <p class="countdown-text">将在 <span id="countdown">3</span> 秒后自动跳转……</p>
+</div>
+<script type="text/javascript">
+    var seconds = 3;
+    var countdown = document.getElementById('countdown');
+    var interval = setInterval(function() {
+        seconds--;
+        countdown.innerHTML = seconds;
+        if (seconds <= 0) {
+            clearInterval(interval);
+            window.location.href = "$redirectUrl";
+        }
+    }, 1000);
+</script>
+</body>
+</html>
+HTML;
+}
 ?>
